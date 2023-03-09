287d7ab1cbd904c98017569ae279697b5f503b66
Ronen Boxer <ronenboxer@gmail.com>
2023-03-09 14:49:26 +0200

    removed default app page and styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,19 @@ import HomePage from './views/HomePage';
 import WapList from './views/WapList';
 import WapEdit from './views/WapEdit';
 import WapPreview from './views/WapPreview';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <Router>
-    <section className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Routes>
-      <Route path='/website/:id/edit' element={ <WapEdit />}/>
-      <Route path='/website/:id' element={ <WapPreview />}/>
-      <Route path='/website' element={ <WapList />}/>
-      <Route path='/' element={ <HomePage />}/>
-      </Routes>
-    </section>
+      <section className="App">
+
+        <Routes>
+          <Route path='/website/:id/edit' element={<WapEdit />} />
+          <Route path='/website/:id' element={<WapPreview />} />
+          <Route path='/website' element={<WapList />} />
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </section>
     </Router>
   );
 }
